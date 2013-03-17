3daf1e50a0b3bf9ab5bf25097597e52961c5116a
Henrik Møller Rasmussen <hmr@agitso.com>
2013-03-17 10:51:23 +0100

    Log also if banning is disabled

--- Classes/Ag/Cache/Service/BanService.php
+++ Classes/Ag/Cache/Service/BanService.php
@@ -34,7 +34,6 @@ class BanService {
 	 */
 	public function ban($url, $recursive = FALSE) {
 		if (array_key_exists('allowBan', $this->settings) && $this->settings['allowBan'] === TRUE) {
-			$this->systemLogger->log('Ban ' . $url);
 			$curlHandle = curl_init();
 
 			curl_setopt($curlHandle, CURLOPT_URL, $url);
@@ -44,6 +43,10 @@ class BanService {
 			curl_exec($curlHandle);
 
 			curl_close($curlHandle);
+
+			$this->systemLogger->log('Banned ' . $url);
+		} else {
+			$this->systemLogger->log('If allowBan was TRUE - would have banned ' . $url);
 		}
 	}
 }
